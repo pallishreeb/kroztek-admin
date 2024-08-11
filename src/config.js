24f03db1a1454576f670db2e66de74962adae757
Pallishree <pallishreebehera01@gmail.com>
2024-08-11 20:37:33 +0530

    new build

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const API_URL = 'http://13.232.38.197/dev/api/v1'
-export const APP_URL = "http://13.232.38.197";
-export const IMG_URL = 'http://13.232.38.197/dev/api/uploads'
+export const API_URL = 'https://kroztek.com/dev/api/v1'
+export const APP_URL = "https://kroztek.com";
+export const IMG_URL = 'https://kroztek.com/dev/api/uploads'
 
 
 // export const API_URL = 'http://localhost:8000/dev/api/v1'
